012eb1ad8965c52f979655bcfd9ab0eb8354483a
David Dlp <dlpdavid17@gmail.com>
2023-01-24 10:12:19 +0100

    Change organize components pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import CategoryPage from "./@pages/CategoryPage";
-import DetailsPage from "./@pages/DetailsPage";
-import FilmsPage from "./@pages/FilmsPage";
-import HomePage from "./@pages/HomePage";
-import SeriesPage from "./@pages/SeriesPage";
-import SubscribePage from "./@pages/SubscribePage";
+import CategoryPage from "./@pages/CategoryPage/CategoryPage";
+import DetailsPage from "./@pages/DetailPage/DetailsPage";
+import FilmsPage from "./@pages/FilmPage/FilmsPage";
+import HomePage from "./@pages/HomePage/HomePage";
+import SeriesPage from "./@pages/SeriesPage/SeriesPage";
+import SubscribePage from "./@pages/SubscribePage/SubscribePage";
 
 export const App = () => {
   return (
